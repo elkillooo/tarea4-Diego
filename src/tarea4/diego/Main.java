a758f036cab5c9aac3a0003cf8670c58e849660a
wantadog <jlopmar196@gmail.com>
2019-02-08 13:19:15 +0100

    Añado clase main

---
+++ src/tarea4/diego/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea4.diego;
+
+/**
+ *
+ * @author chemorrow
+ */
+public class Main {
+    public static void main(String[] args) {
+        Tanqueray tanke1 = new Tanqueray("48%", 9);
+        Tanqueray tanke2 = new Tanqueray("45%", 7);
+        
+        System.out.println(tanke1);
+        System.out.println(tanke2);
+    }
+}
